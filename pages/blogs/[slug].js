e4d036dd439f64330022d7a0f4db117dd696a71f
Deep Shah <deepshah1358@gmail.com>
2021-04-17 10:49:35 +0530

    Fallback is warning

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -3,29 +3,35 @@ import BasePage from "../../components/BasePage";
 import { useGetUser } from "../../actions/user";
 import BlogApi from "../../lib/api/blogs";
 import { Col, Row } from "reactstrap";
+import { useRouter } from "next/router";
 
 import Avatar from "../../components/shared/Avatar";
 
 const BlogDetail = ({ blog, author }) => {
+  const router = useRouter();
+
   const { data, error, loading } = useGetUser();
   return (
     <BaseLayout user={data} loading={loading}>
       <BasePage
-        metaDescription={blog.subTitle}
-        title={`${blog.title} - Deep Shah`}
+        metaDescription={blog?.subTitle || ""}
+        title={`${blog?.title || ""} - Deep Shah`}
       >
         <Row>
-          <Col md={{ size: 8, offset: 2 }}>
-            <Avatar
-              image={author.picture}
-              title={author.name}
-              date={blog.createdAt}
-            />
-            <hr />
-            <h1>{blog.title}</h1>
-            <h2>{blog.subTitle}</h2>
-            <p>{blog.content}</p>
-          </Col>
+          {router.isFallback && <h1>Your Page is getting Served</h1>}
+          {!router.isFallback && (
+            <Col md={{ size: 8, offset: 2 }}>
+              <Avatar
+                image={author.picture}
+                title={author.name}
+                date={blog.createdAt}
+              />
+              <hr />
+              <h1>{blog.title}</h1>
+              <h2>{blog.subTitle}</h2>
+              <p>{blog.content}</p>
+            </Col>
+          )}
         </Row>
       </BasePage>
     </BaseLayout>
@@ -36,9 +42,8 @@ export default BlogDetail;
 
 export async function getStaticPaths() {
   const { data } = await new BlogApi().getAll();
-  // const blogs = json.data;
   const paths = data.map(({ blog }) => ({ params: { slug: blog.slug } }));
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
