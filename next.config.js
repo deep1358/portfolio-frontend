ee8544384d15df06651372068794d9b39997be8a
Deep Shah <deepshah1358@gmail.com>
2022-10-16 17:21:50 +0530

    Temp

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  env:{
-    AUTH0_NAMESPACE:process.env.AUTH0_NAMESPACE,
-    BASE_URL:process.env.BASE_URL
-  }
-}
+	env: {
+		AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE,
+		BASE_URL: process.env.BASE_URL,
+	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+};
